4a4e4d79a463c4a318381801ce4da9720057ef44
mistachatty-cmyk <mistachatty@gmail.com>
2026-09-01 18:51:03 -0400

    Load complete Looper signature FX

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import './custom-scenarios.css';
 import './customizations.css';
 import './pixel-pets.css';
 import './looper-animations.css';
+import './looper-signature-fx.css';
 import './looper-showcase.css';
 import './lokdex.css';
 import './card-shop.css';
